c22fcf1cf865e0963ca4285f9205a2f3c4a2817d
Hillarydalie <hidalie@gmail.com>
2020-05-05 21:14:55 +0300

    Add database settings

--- momento/settings.py
+++ momento/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'momento',
-        'USER': 'admin',
+        'USER': 'dalie',
     'PASSWORD':'password',
     }
 }
